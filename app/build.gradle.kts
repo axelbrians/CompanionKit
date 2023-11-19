374c4c746316b04eac271c2cf42171aa85fdc6fb
axelbrians <axelbrians@gmail.com>
2023-11-19 16:10:18 +0700

    [Chore] Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.agp)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "org.axelbrians.companionkit"
+    compileSdk = libs.versions.sdk.compile.getAsInt()
+
+    defaultConfig {
+        applicationId = "org.axelbrians.companionkit"
+        minSdk = libs.versions.sdk.min.getAsInt()
+        targetSdk = libs.versions.sdk.target.getAsInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvm.get()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.activity)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.graphics)
+    implementation(libs.compose.tooling.preview)
+    implementation(libs.compose.material3)
+
+    debugImplementation(libs.compose.tooling)
+    debugImplementation(libs.test.compose.manifest)
+
+    testImplementation(libs.test.junit)
+
+    androidTestImplementation(libs.test.androidx.junit)
+    androidTestImplementation(libs.test.androidx.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.test.compose.junit4)
+}
+
+fun org.gradle.api.provider.Provider<String>.getAsInt(): Int {
+    return this.get().toInt()
+}
